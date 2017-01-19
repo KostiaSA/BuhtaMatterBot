6daec9262138bc9d3f0ee95b5abaa844d2e2471e
KostiaSA <sonyk795>
2017-01-19 17:09:54 +0300

    --

--- scripts/_registerAllScripts.ts
+++ scripts/_registerAllScripts.ts
@@ -3,10 +3,12 @@ import {Привет_script} from "./привет";
 import {BotScript} from "../BotScript";
 import {getInstantPromise} from "../utils/getInstantPromise";
 import {Help_script} from "./help";
+import {Прайс_script} from "./прайс";
 
 export let registeredBotScripts:BotScript[]=[
     new Help_script(),
-    new Привет_script()
+    new Привет_script(),
+    new Прайс_script(),
 ];
 
 export async function bot(userName: string, message: string): Promise<string> {
